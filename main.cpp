b3c306a342b4dcddfc2f8f29b55b9497cea40fc3
GFS Geokar <geokarjesos@gmail.com>
2026-05-14 16:57:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
                               Online C++ Compiler.
                Code, Compile, Run and Debug C++ program online.
 Write your code in this editor and press "Run" button to compile and execute it.
-				XenOptimzeCd:TOADAMVWZPBIO#KT63_$?$5_@@#MILVBG&?_
+				XenOptimzeCd:TOADAMVWZPBIO#KT63_$?$5_@@#MILVBG
 
 *******************************************************************************/
 
